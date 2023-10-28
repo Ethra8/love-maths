dae086f7cf3b54ed87b3f2d4b14efddcf6b4e37f
Ethra8 <80659091+Ethra8@users.noreply.github.com>
2023-10-28 20:51:34 +0200

    add code to check user answer and respond to the user accordingly

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     for(let button of buttons){
         button.addEventListener('click', function(){
             if(this.getAttribute("data-type") === "submit"){
-                alert("You clicked submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -37,13 +37,40 @@ function runGame(gameType) {
     }
 }
 
-
+/**
+ * Checks the user answer against the 1st element in the returned
+ * calculateCorrectAnswer() array
+ */
 function checkAnswer() {
 
-}
+    let userAnswer = parseInt(document.getElementById('answer-box').value); //value instead of .innerText, as input type is set to type=number
+    let calculatedAnswer = calculateCorrectAnswer(); //function returns an array [result , "next game type"]
+    let isCorrect = userAnswer === calculatedAnswer[0]; //index 0 of array is result (1st parameter of array), will return TRUE or FALSE
 
+    if (isCorrect) { //short way of saying if isCorrect == true
+        alert("Hey, You got it right! :D");
+    } else {
+        alert(`Awwwwww... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+    }
+
+    runGame(calculatedAnswer[1]); //index 1 of array is the same game type, which is the default next game type 
+}   
+
+/**
+ * Gets the operands (numbers) and the operator (plus, minus, etc)
+ * directly from the DOM, and returns correct answer.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText); //parseInt returns integer, instead of default string. Can't make operations from string, but CAN from integers (numbers)
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
+    if (operator === "+"){
+        return [operand1 + operand2, "addition"]; //returns result of operand1 + operand2, and predetermines the next game: addition
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
@@ -55,9 +82,10 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
-document.getElementById('operand1').textContent = operand1;
-document.getElementById('operand2').textContent = operand2;
-document.getElementById('operator').textContent = "+";
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+
 }
 
 function displaySubtractQuestion() {
